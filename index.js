f0199b144508427a8ae62a2368465317e634ee34
mixmix <whimful@gmail.com>
2020-03-05 14:58:19 +1300

    add package.json entrypoint for publishing

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  cloaked_msg_id: require('./cloaked_msg_id/constants.json'),
+  derive_secret: require('./derive_secret/constants.json'),
+  tfk: require('./encoding/tfk.json')
+}
