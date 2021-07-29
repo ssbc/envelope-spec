52dad9547ccd6f69c71222cf1af3a8188541e279
Anders Rune Jensen <arj03@protonmail.ch>
2021-07-29 21:35:06 +0200

    Introduce BFE

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 module.exports = {
   cloaked_msg_id: require('./cloaked_msg_id/constants.json'),
-  derive_secret: require('./derive_secret/constants.json'),
-  tfk: require('./encoding/tfk.json')
+  derive_secret: require('./derive_secret/constants.json')
 }
