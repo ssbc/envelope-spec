77abd21c2e4d5296d5e2ed0ba03fbaa3def6c3d6
Anders Rune Jensen <arj03@protonmail.ch>
2021-08-02 08:47:37 +0200

    Export TFK from ssb-bfe

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const bfe = require('ssb-bfe')
+
 module.exports = {
   cloaked_msg_id: require('./cloaked_msg_id/constants.json'),
-  derive_secret: require('./derive_secret/constants.json')
+  derive_secret: require('./derive_secret/constants.json'),
+  tfk: bfe.bfeTypes,
+  bfe
 }
